00cb103f1a67112bbb8150aefeff8bb92d5c0bdd
Furkan Alp <furkanalptokac@gmail.com>
2020-12-20 03:05:14 +0300

    project structure changed completely

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit HELLO WORLD<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React QWEQWE
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <h1>App</h1>
+  </Fragment>
+);
 
 export default App;
