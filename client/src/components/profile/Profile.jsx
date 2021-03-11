15487672fc32fa52be311acb0ecb66c317103766
Furkan Alp <furkanalptokac@gmail.com>
2021-03-11 12:17:29 +0300

    display user profile details

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,20 +3,34 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import ProfileTop from './ProfileTop'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
     useEffect(() => {
         getProfileById(match.params.id);
     }, [getProfileById, match.params.id]);
-
     return (
         <Fragment>
-            {profile === null || loading ? <Spinner /> : <Fragment>
-                <Link to="/profiles" className="btn btn-light">
-                    Profillere geri dön
-                </Link>
-            </Fragment>}
+            {profile === null || loading ? (
+            <Spinner />
+            ) : (
+                <Fragment>
+                    <Link to="/profiles" className="btn btn-light">
+                        Profillere geri dön
+                    </Link>
+                    {auth.isAuthenticated &&
+                    auth.loading === false &&
+                    auth.user._id === profile.user._id && (
+                        <Link to="/edit-profile" className="btn btn-dark">
+                            Profili Düzenle
+                        </Link>
+                    )}
+                    <div className="profile-grid my-1">
+                        <ProfileTop profile={profile} />
+                    </div>
+                </Fragment>
+            )}
         </Fragment>
     )
 }
---
+++ client/src/components/profile/ProfileTop.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProfileTop = ({
+    profile: {
+        location,
+        social,
+        bio,
+        user: { name, avatar }
+    }
+}) => {
+    return (
+        <div className="profile-top bg-primary p-2">
+            <img className="round-img my-1" src={avatar} alt=""/>
+            <h1 className="large">{name}</h1>
+            <p className="lead">
+                {bio && <span>{bio}</span>}
+            </p>
+            <p>{location && <span>{location}</span>}</p>
+
+            <div className="icons my-1">
+                {social && social.facebook && (
+                    <a href={social.facebook} target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                )}
+                {social && social.twitter && (
+                    <a href={social.twitter} target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                )}
+                {social && social.instagram && (
+                    <a href={social.instagram} target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                )}
+            </div>
+        </div>
+    )
+}
+
+ProfileTop.propTypes = {
+    profile: PropTypes.object.isRequired,
+}
+
+export default ProfileTop
