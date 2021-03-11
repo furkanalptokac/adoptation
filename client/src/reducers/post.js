fdec3e02404db315d3f0f109c60a34be4da63dd1
Furkan Alp <furkanalptokac@gmail.com>
2021-03-11 21:28:38 +0300

    post reducer

---
+++ client/src/reducers/post.js
@@ -0,0 +1,30 @@
+import {
+    GET_POSTS,
+    POST_ERROR
+} from '../../actions/types'
+
+const initialState = {
+    posts: [],
+    post: null,
+    loading: true,
+    error: {}
+}
+
+export default functtion (state = initialState, action) {
+    const { type, payload } = action
+
+    switch (type) {
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: payload,
+                loading: false
+            }
+        case POST_ERROR:
+            return {
+                ...state,
+                error: payload,
+                loading: false
+            }
+    }
+}
