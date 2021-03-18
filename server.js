cc8363d9ab8faaa6746ef4e5ad05e304fdee318a
Furkan Alp <furkanalptokac@gmail.com>
2021-03-18 11:44:12 +0300

    fix

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(app.get('port'), () => {
+    console.log(`Server is running on http://localhost:${app.get('port')}`);
 });
