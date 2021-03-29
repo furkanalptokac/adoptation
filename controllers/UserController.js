5d869340a650da2ec3e95714c14dcc9e0d4aeb09
Furkan Alp <furkanalptokac@gmail.com>
2021-03-29 10:41:16 +0300

    user api descriptions + controller fixes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,6 +18,7 @@ schema
 .is().max(100)
 .has().uppercase()
 .has().lowercase()
+.has().digits(1)
 
 exports.signup = async (req, res) => {
     const errors = validationResult(req);
@@ -97,65 +98,6 @@ exports.signup = async (req, res) => {
     }
 }
 
-exports.findFromEmail = async (req, res) => {
-    let user = await User.find({
-        email: req.body.email
-    });
-
-    if (!user) {
-        return res.status(400).send('Bu email ile kullanici bulunamadi.');
-    } else {
-        res.send(user);
-    }
-}
-
-exports.findFromId = async (req, res) => {
-    User.findById(req.params.id, function (err, user) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(user);
-        }
-    })
-}
-
-exports.findAllUsers = async (req, res) => {
-    await User.find({}, function (err, users) {
-        var userMap = {};
-
-        users.forEach(function (user) {
-            userMap[user._id] = user;
-        });
-
-        res.send(userMap);
-    });
-}
-
-exports.deleteFromEmail = async (req, res) => {
-    User.deleteOne({ 
-        email: req.body.email
-    }).then(function () {
-        res.send('Silindi');
-    }).catch(function (err) {
-        res.send(err);
-    });
-}
-
-exports.updateUser = async (req, res) => {
-    await User.findByIdAndUpdate(req.params.id, {
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: req.body.password,
-        city: req.body.city
-    }, function (err, result) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
-    });
-}
 
 exports.updatePassword = async (req, res) => {
     try {
@@ -177,68 +119,42 @@ exports.updatePassword = async (req, res) => {
 }
 
 exports.forgotPassword = async (req, res) => {
-    let password = passwordGenerator.generate({
-        length: 10,
-        numbers: true
-    });
+    try {
+        let password = passwordGenerator.generate({
+            length: 10,
+            numbers: true
+        });
 
-    let transporter = nodemailer.createTransport({
-        host: process.env.HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_PW
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+        let hashedPassword = bcrypt.hashSync(password, 10);
 
-    let mailOptions = {
-        from: process.env.EMAIL,
-        to: req.body.email,
-        subject: 'Sahiplenn Destek',
-        text: 'Şifreniz başarıyla sıfırlanmıştır. Yeni şifreniz ' + password
-    };
+        await User.findOneAndUpdate({ email: req.body.email }, { password: hashedPassword});
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error.message);
-        }
-        console.log('Message sent: %s', info.messageId);
-    });
+        let transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.EMAIL_PW
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
 
-    let hashedPassword = bcrypt.hashSync(password, 10);
+        let mailOptions = {
+            from: process.env.EMAIL,
+            to: req.body.email,
+            subject: 'Sahiplenn Destek',
+            text: 'Parolanız başarıyla sıfırlanmıştır. Yeni parolanız: ' + password
+        };
 
-    await User.findOneAndUpdate({ email: req.body.email }), {
-        password: hashedPassword
-    }, (err, result) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error.message);
+            }
+            console.log('Email başarıyla gönderildi. : %s', info.messageId);
+        });
+    } catch (err) {
+        console.error(err)
     }
-}
-
-exports.deleteFromId = async (req, res) => {
-    await User.findByIdAndRemove(req.params.id, function (err, result) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
-    })
-}
-
-exports.followPost = async (req, res) => {
-    let user = await User.findById(req.params.id);
-    let posts = user.posts + ',' + req.body.id; 
-    await User.findByIdAndUpdate(req.params.id, { posts: posts }, function (err, result) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
-    })
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,24 +2,33 @@ const router = require('express').Router();
 const UserController = require('../../controllers/UserController');
 const validator = require('../../controllers/validator');
 
+/** 
+ * @api {post} /api/posts/signup Sign up
+ * @apiName Sign up
+ * @apiGroup User
+ * @apiParam {String} Name
+ * @apiParam {String} Surname
+ * @apiParam {String} Email
+ * @apiParam {String} Password
+ */
 router.post('/signup', validator.register, UserController.signup);
 
-router.get('/finduser', UserController.findFromEmail);
-
-router.get('/getallusers', UserController.findAllUsers);
-
-router.get('/findfromid/:id', UserController.findFromId);
-
-router.post('/followpost/:id', UserController.followPost);
-
-router.put('/updateuser/:id', UserController.updateUser);
-
+/** 
+ * @api {put} /api/posts/updatepassword/:id Update password
+ * @apiName Update password
+ * @apiGroup User
+ * @apiParam {id} User id
+ * @apiParam {String} Password
+ */
 router.put('/updatepassword/:id', UserController.updatePassword);
 
+/** 
+ * @api {put} /api/posts/forgotpassword Reset password
+ * @apiName Reset password
+ * @apiGroup User
+ * @apiParam {id} User id
+ * @apiParam {String} Password
+ */
 router.put('/forgotpassword', UserController.forgotPassword);
-
-router.delete('/deleteuser', UserController.deleteFromEmail);
-
-router.delete('/deletefromid/:id', UserController.deleteFromId);
 
 module.exports = router;
